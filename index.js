ad82c205e34cbee8a04f7546a58ecee971056807
muradwahid <muradwahid334@gmail.com>
2022-11-19 17:41:28 +0600

    food category api created

--- index.js
+++ index.js
@@ -18,7 +18,43 @@ async function run() {
       serverApi: ServerApiVersion.v1,
     });
 
+    const data = client.db("assignment");
+    const foodsCollection = data.collection("foods");
+    const reviewCollection = data.collection("review");
+    app.get("/category", async (req, res) => {
+      const query = {};
+      const cursor = foodsCollection.find(query);
+      const foods = await cursor.limit(3).toArray();
+      res.send(foods);
+    });
     
+          app.post("/services", async (req, res) => {
+            const data = req.body;
+            const result = await foodsCollection.insertOne(data);
+            res.send(result);
+          });
+
+
+    app.get("/foods/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const food = await foodsCollection.findOne(query);
+      res.send(food);
+    });
+    //   review
+      app.get('/review', async (req, res) => {
+          const query = {};
+          const cursor = reviewCollection.find(query);
+          const review = await cursor.toArray();
+          res.send(review);
+      })
+    app.get('/myreview', async (req, res) => {
+      let query = {};
+      if (req.query.email) {
+        query = {
+          email:req.query.email
+        }
+      }
       const cursor = reviewCollection.find(query);
       const review = await cursor.toArray();
       res.send(review);
